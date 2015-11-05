45eafdd3a276b00a4bbce6157ac2a53c842d7d8e
Ryan Tablada <ryan.tablada@gmail.com>
2015-11-05 13:39:28 -0600

    Add simpsons

---
+++ simpsons.js
@@ -0,0 +1,31 @@
+var characters = [
+	{firstName: 'Lisa', lastName: 'Simpson', age: 10, parent: ['Homer', 'Marge'], hair: 'blond' },
+	{firstName: 'Bart', lastName: 'Simpson', age: 9, parent: ['Homer', 'Marge'], hair: 'blond' },
+	{firstName: 'Maggie', lastName: 'Simpson', age: 2, parent: ['Homer', 'Marge'], hair: 'black' },
+
+	{firstName: 'Patty', lastName: 'Bouvier', age: 37, parent: ['Jacqueline'], hair: 'blond' },
+	{firstName: 'Selma', lastName: 'Bouvier', age: 38, parent: ['Jacqueline'], hair: 'blond' },
+
+	{firstName: 'Rod', lastName: 'Flanders', age: 9, parent: ['Ned'], hair: 'blond' },
+	{firstName: 'Todd', lastName: 'Flanders', age: 8, parent: ['Ned'], hair: 'blond' },
+
+	{firstName: 'Abe', lastName: 'Simpson', age: 65, hair: 'none' },
+	{firstName: 'Mona', lastName: 'Simpson', age: 65, hair: 'none' },
+	{firstName: 'Jacqueline', lastName: 'Bouvier', age: 63, hair: 'none' },
+	{firstName: 'Homer', lastName: 'Simpson', age: 42, parent: ['Abe', 'Mona'], hair: 'none' },
+	{firstName: 'Marge', lastName: 'Simpson', age: 35, parent: ['Jacqueline'], hair: 'blond' },
+	{firstName: 'Ned', lastName: 'Flanders', age: 40, hair: 'none' },
+
+	{firstName: 'Apu', age: 40, lastName: 'Nahasapeemapetilon', hair: 'black' },
+	{firstName: 'Manjula', lastName: 'Nahasapeemapetilon', age: 40, hair: 'brown' },
+];
+
+// Find the just the members of the "Simpsons" family
+
+// Find just the children of Marge and Homer
+
+// Calculate the max age
+
+// Calculate the average age
+
+// List the number of children for each person
