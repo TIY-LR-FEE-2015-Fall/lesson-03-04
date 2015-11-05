da30a53b13334b4b909277db606365eba352722f
Ryan Tablada <ryan.tablada@gmail.com>
2015-11-05 11:42:12 -0600

    Use logic || statements for vowels

--- exercises.js
+++ exercises.js
@@ -31,11 +31,14 @@ function maxOfAll() {
 
 function isVowel(char) {
   'use strict';
-  //...
+
+  return char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u';
 }
 
 // ---------------------
-// Write a function translate() that will translate a text into "rövarspråket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
+// Write a function translate() that will translate a text into "rövarspråket".
+// That is, double every consonant and place an occurrence of "o" in between.
+// For example, translate("this is fun") should return the string "tothohisos isos fofunon".
 // ---------------------
 
 function rovarspraket(phrase) {
